9f838924129d8721def53e4fcc4469c3c3f0d89f
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-05-18 23:35:06 +0900

    rspec導入

---
+++ spec/rails_helper.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+
+ENV['RAILS_ENV'] ||= 'test'
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.include FactoryBot::Syntax::Methods
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
