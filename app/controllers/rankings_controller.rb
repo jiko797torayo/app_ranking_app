eaed6391993dda9331997a89b82743cc6fce0ebe
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-05-19 22:59:55 +0900

    countryをnilガード

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,7 +1,8 @@
 class RankingsController < ApplicationController
   layout 'rankings'
 
-  def get_app_ranking_data(country: 'us', type:, limit:)
+  def get_app_ranking_data(country:, type:, limit:)
+    country ||= 'US'
     uri = URI.parse("https://rss.itunes.apple.com/api/v1/#{country}/ios-apps/#{type}/all/#{limit}/explicit.json")
     json = Net::HTTP.get(uri)
     JSON.parse(json)['feed']['results']
