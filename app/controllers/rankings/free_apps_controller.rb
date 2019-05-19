74a932362f6838c91f65b3c592fbdb41e1d25ebe
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-05-19 20:38:22 +0900

    Rspec導入

--- app/controllers/rankings/free_apps_controller.rb
+++ app/controllers/rankings/free_apps_controller.rb
@@ -1,7 +1,6 @@
 class Rankings::FreeAppsController < RankingsController
   def index
     @apps = StoreApi::AppStore::Apps::Ranking.new('topfreeapplications', '36', params[:country], 10).topchart
-    @countries = Country.all
+    @countries = Country.all.order(name: :asc)
   end
 end
-
