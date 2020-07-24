f82440a447ef712d9e94548884cf723ebd80bd04
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-07-24 16:33:13 +0900

    add/プライバシーポリシー

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/health', controller: :elb, action: :health
+  get '/privacy_policy', controller: :privacy_policy, action: :index
   root to: 'rankings#index'
   resources :rankings, only: [:index]
 end
